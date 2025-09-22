5eb6f6fe00d4a51ae2842e7a40dcd20e03e28f30
Shamik Bera <berashamik115@gmail.com>
2025-09-22 20:36:09 +0530

    Project INIT

---
+++ patient-service/src/main/java/com/pm/patient_service/model/Patient.java
@@ -0,0 +1,39 @@
+package com.pm.patient_service.model;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
+// continue to watch from 35:29
+
+@Entity
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    private String address;
+
+    @NotNull
+    private LocalDate dateOfBirth;
+
+    @NotNull
+    private LocalDate registeredDate;
+
+}
