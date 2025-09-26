7a997c3a08dff4d03addd651f26ad020a582c3d9
Shamik Bera <berashamik115@gmail.com>
2025-09-26 20:53:24 +0530

    feat: Implement PatientController and enhance PatientService for patient management

---
+++ patient-service/src/main/java/com/pm/patient_service/controllers/PatientController.java
@@ -0,0 +1,41 @@
+package com.pm.patient_service.controllers;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pm.patient_service.dto.PatientRequestDTO;
+import com.pm.patient_service.dto.PatientResponseDTO;
+import com.pm.patient_service.service.PatientService;
+
+import jakarta.validation.Valid;
+
+// continue from 1:42:47
+
+@RestController
+@RequestMapping("/patients")
+public class PatientController {
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PatientResponseDTO>> getPatients() {
+        List<PatientResponseDTO> patients = patientService.getPatients();
+        return ResponseEntity.ok().body(patients);
+    }
+
+    @PostMapping
+    public ResponseEntity<PatientResponseDTO> createPatient(@Valid @RequestBody PatientRequestDTO patientRequestDTO) {
+        PatientResponseDTO patientResponseDTO = patientService.createPatient(patientRequestDTO);
+        return ResponseEntity.ok().body(patientResponseDTO);
+    }
+
+}
--- patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
@@ -4,29 +4,28 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.pm.patient_service.dto.PatientRequestDTO;
 import com.pm.patient_service.dto.PatientResponseDTO;
 import com.pm.patient_service.mapper.PatientMapper;
 import com.pm.patient_service.model.Patient;
 import com.pm.patient_service.repository.PatientRepository;
 
-
-// watch from 1:12:50
-
 @Service
 public class PatientService {
-    private  PatientRepository patientRepository;
-    
+    private PatientRepository patientRepository;
+
     public PatientService(PatientRepository patientRepository) {
         this.patientRepository = patientRepository;
     }
 
     public List<PatientResponseDTO> getPatients() {
         List<Patient> patients = patientRepository.findAll();
-        List<PatientResponseDTO> patientResponseDTOs = patients.stream().map(PatientMapper::toDTO).toList();
-        return patientResponseDTOs;
+        return patients.stream().map(PatientMapper::toDTO).toList();
     }
 
-
-
+    public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO) {
+        Patient newPatient = patientRepository.save(PatientMapper.toModel(patientRequestDTO));
+        return PatientMapper.toDTO(newPatient);
+    }
 
 }
