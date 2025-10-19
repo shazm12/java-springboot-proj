be28b8c00654cd87b1df4dd658eb560c37c0c6f9
Shamik Bera <berashamik115@gmail.com>
2025-10-19 11:35:04 +0530

    feat: Add validation group for registered date in PatientRequestDTO to enforce required field constraints

--- patient-service/src/main/java/com/pm/patient_service/dto/PatientRequestDTO.java
+++ patient-service/src/main/java/com/pm/patient_service/dto/PatientRequestDTO.java
@@ -1,5 +1,7 @@
 package com.pm.patient_service.dto;
 
+import com.pm.patient_service.dto.validators.CreatePatientValidationGroup;
+
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -20,7 +22,7 @@ public class PatientRequestDTO {
     @NotBlank(message = "Date of Birth is required.")
     private String dateOfBirth;
 
-    @NotNull(message = "Registered Date is required.")
+    @NotBlank(groups=CreatePatientValidationGroup.class, message = "Registered Date is required.")
     private String registeredDate;
 
     public String getName() {
