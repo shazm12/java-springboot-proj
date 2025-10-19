c42a66b852ac954763620136c92066eb5c16c569
Shamik Bera <berashamik115@gmail.com>
2025-10-19 11:35:31 +0530

    feat: Add PatientNotFoundException and enhance GlobalExceptionHandler to manage patient not found errors

--- patient-service/src/main/java/com/pm/patient_service/exception/GlobalExceptionHandler.java
+++ patient-service/src/main/java/com/pm/patient_service/exception/GlobalExceptionHandler.java
@@ -30,4 +30,12 @@ public class GlobalExceptionHandler {
         return ResponseEntity.badRequest().body(errors);
     }
 
+    @ExceptionHandler(PatientNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handlePatientNotFoundException(PatientNotFoundException ex) {
+        log.warn("Patient Not Found {}", ex.getMessage());
+        Map<String,String> errors = new HashMap<>();
+        errors.put("message", "Patient Not Found");
+        return ResponseEntity.badRequest().body(errors);
+    }
+
 }
