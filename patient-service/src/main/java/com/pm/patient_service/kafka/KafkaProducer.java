f3987a1e0b02d05b4a631105429426e9b46d3dff
Shamik Bera <berashamik115@gmail.com>
2025-11-27 20:42:39 +0530

    chore: add a method sendEvent for sending patient events from producer;

--- patient-service/src/main/java/com/pm/patient_service/kafka/KafkaProducer.java
+++ patient-service/src/main/java/com/pm/patient_service/kafka/KafkaProducer.java
@@ -1,12 +1,34 @@
 package com.pm.patient_service.kafka;
 
+import com.pm.patient_service.model.Patient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+@Service
 public class KafkaProducer {
 
+    private static final Logger log = LoggerFactory.getLogger(KafkaProducer.class);
     private final KafkaTemplate<String, byte[]> kafkaTemplate;
 
     public KafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendEvent(Patient patient) {
+        Patient event = PatientEvent.newBuilder()
+            .setPatientId(patient.getId().toString())
+            .setName(patient.getName())
+            .setEmail(patient.getEmail())
+            .setEventType("PAITENT_CREATED")
+            .build();
+        
+            try {
+                kafkaTemplate.send("patient", event.toByteArray());
+            }
+            catch(Exception e) {
+                log.error("Error sending PatientCreated event: {}", event);
+            } 
+        
     }
 
 
