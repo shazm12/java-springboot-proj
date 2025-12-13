7588f544e778d73ebe510a993fe97dec45fbc7db
Shamik Bera <berashamik115@gmail.com>
2025-12-13 15:19:24 +0530

    feat: implement token validation endpoint in AuthController

--- auth-service/src/main/java/com/example/authservice/auth_service/controller/AuthController.java
+++ auth-service/src/main/java/com/example/authservice/auth_service/controller/AuthController.java
@@ -2,19 +2,30 @@ package com.example.authservice.auth_service.controller;
 
 import java.util.Optional;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.authservice.auth_service.dto.LoginRequestDTO;
 import com.example.authservice.auth_service.dto.LoginResponseDTO;
+import com.example.authservice.auth_service.service.AuthService;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class AuthController {
+    private final AuthService authService;
+
+    public AuthController() {
+        this.authService = authService;
+    }
 
     @Operation(summary = "Generate token on user login")
     @PostMapping("/login")
@@ -28,6 +39,16 @@ public class AuthController {
         String token = tokenOptional.get();
         return ResponseEntity.ok(new LoginResponseDTO(token)):
 
+    }
+
+    @Operation(summary = "Validate token")
+    @GetMapping("/vaildate")
+    public ResponseEntity<Void> validateToken(@RequestHeader("Authorization") String authHeader) {
+        if (authHeader == null || !authheader.startsWith("Bearer ")) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        return authService.validateToken(authHeader.substring(7)) ? ResponseEntity.ok().build()
+                : ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 
 }
