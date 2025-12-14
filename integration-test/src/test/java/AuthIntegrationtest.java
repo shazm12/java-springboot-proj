f72f19ed88733fcdca9a1f83f5a3a3c4a39d3cbc
Shamik Bera <berashamik115@gmail.com>
2025-12-14 16:28:24 +0530

    feat: add AuthIntegrationTest for authentication flow validation;

---
+++ integration-test/src/test/java/AuthIntegrationtest.java
@@ -0,0 +1,41 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.given;
+import io.restassured.RestAssured;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class AuthIntegrationTest {
+
+    @BeforeAll
+    static void setUp() {
+        RestAssured,basURI = "http://localhost:4004";
+    }
+
+    @Test
+    public void shouldReturnOkWithValidToken() {
+
+        String loginPayload = """
+                        {
+                            "email": "testuser@test.com",
+                            "password": "password123"
+                        }
+                """;
+
+        Response reponse = given()
+                .contentType(ContentType.JSON)
+                .body(loginPayload)
+                .when()
+                .post("/auth/login")
+                .then()
+                .statusCode(200)
+                .body("token", notNullValue())
+                .extract()
+                .response();
+
+        System.out.println("Generated Token: " + reponse.jsonPath().getString("token"));
+
+    }
+
+}
